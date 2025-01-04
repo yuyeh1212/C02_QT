6b986d001c34ca097f75adbf6e020f492cf0335e
yuyeh <49071263+yuyeh1212@users.noreply.github.com>
2025-01-04 14:19:29 +0800

    環境建置

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./assets/style/style.scss";
+import { Modal } from "bootstrap";
+//import "./index.css";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
