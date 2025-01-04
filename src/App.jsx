f1da1bfcc69160293430ca353333e73746403f7c
dieboss <s41115208@gmail.com>
2025-01-04 15:13:38 +0800

    bs5顏色初始化設定

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,8 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <div className="card linear">
+        <button className='btn btn-secondary-100' onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
