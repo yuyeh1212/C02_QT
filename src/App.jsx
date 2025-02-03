1fd3fbf31aaed21875628200016296245137e5ed
a1phaki <jok.jok.875@gmail.com>
2025-02-04 01:20:01 +0800

    router初始化設定，新增pages資料夾

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Homepage';
+import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Member from './pages/Member';
