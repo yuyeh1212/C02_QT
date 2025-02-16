fc495b3d2577286c8c58c9b3f5002ba7eec68664
a1phaki <jok.jok.875@gmail.com>
2025-02-17 00:49:42 +0800

    更新footer手機版排版

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,23 +6,25 @@ export default function Footer() {
   return (
     <footer className="bg-primary-02">
       <div className="container">
-        <div className="row py-12 d-flex align-items-stretch">
-          <div className="col-6">
-            <div className="d-flex align-items-stretch">
+        <div className="row py-12 d-flex align-items-center g-4">
+          <div className="col-12 col-md-3 d-flex justify-content-center">
+            <div className="h-100">
               <img
                   src="/logo.png"
                   alt="Quality Time Logo"
                 />
-              <div className="ms-5 d-flex flex-column justify-content-between text-white">
-                <p>店家地址：931 屏東縣佳冬鄉光明路16號</p>
-                <p>連絡電話：0968386848</p>
-                <p>營業時間：10:30-22:00</p>
-              </div>
             </div>
           </div>
-          <div className="col-6">
+          <div className="col-12 col-md-6 d-flex justify-content-center">
+            <div className="ms-5 text-white text-md-start text-center">
+                <p>店家地址：931 屏東縣佳冬鄉光明路16號</p>
+                <p className="py-5">連絡電話：0968386848</p>
+                <p>營業時間：10:30-22:00</p>
+            </div>
+          </div>
+          <div className="col-12 col-md-3">
             {/* 右側社群連結 */}
-            <div className="float-end d-flex align-items-center h-100">
+            <div className="float-md-end d-flex align-items-center justify-content-center ">
               <a to="#" >
                 <Icon icon="ic:baseline-facebook" width="64" height="64"  style={{color: "#fff"}} />
               </a>
