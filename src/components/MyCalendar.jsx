170f380569dbc628f6dc31d1a26f26c4832d38a2
dieboss <s41115208@gmail.com>
2025-02-20 21:06:17 +0800

    新增日歷API +radio元件+日期及選單input

--- src/components/MyCalendar.jsx
+++ src/components/MyCalendar.jsx
@@ -3,36 +3,59 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import { forwardRef, useEffect, useState, useImperativeHandle, useRef } from "react";
 import PropTypes from "prop-types";
+import axios from "axios";
 
-const MyCalendar = forwardRef(({ onDateChange }, ref) => {
+const API_URL = import.meta.env.VITE_API_URL
+
+const MyCalendar = forwardRef(({ onDateChange ,handleCalendar,windowSize,getCalendarInfo}, ref) => {
 
     const [selectedEventId, setSelectedEventId] = useState(null);
+    const [eventDate,setEventDate] = useState([])
     const [events, setEvents] = useState([
-      { id: "1", title: "10:30~14:30", date: "2025-02-19", backgroundColor: "#F7F0EA", textColor: "#6E5E57", classNames: ['custom-event'] },
-      { id: "2", title: "14:30~18:30", date: "2025-02-19", backgroundColor: "#F7F0EA", textColor: "#6E5E57", classNames: ['custom-event'] },
-      { id: "3", title: "18:30~22:30", date: "2025-02-19", backgroundColor: "#F7F0EA", textColor: "#6E5E57", classNames: ['custom-event'] },
-      { id: "4", title: "10:30~14:30", date:"2025-02-20", backgroundColor: "#F7F0EA", textColor: "#6E5E57",classNames: ['custom-event'] },
-      { id: "5", title: "14:30~18:30", date:"2025-02-20", backgroundColor: "#F7F0EA", textColor: "#6E5E57",classNames: ['custom-event'] },
-      { id: "6", title: "18:30~22:30", date:"2025-02-20", backgroundColor: "#F7F0EA", textColor: "#6E5E57",classNames: ['custom-event'] },
+    //   { id: "1", title: "10:30~14:30", date: "2025-02-19", backgroundColor: "#F7F0EA", textColor: "#6E5E57", classNames: ['custom-event'] },
+    //   { id: "2", title: "14:30~18:30", date: "2025-02-19", backgroundColor: "#F7F0EA", textColor: "#6E5E57", classNames: ['custom-event'] },
+    //   { id: "3", title: "18:30~22:30", date: "2025-02-19", backgroundColor: "#F7F0EA", textColor: "#6E5E57", classNames: ['custom-event'] },
+    //   { id: "4", title: "10:30~14:30", date:"2025-02-20", backgroundColor: "#F7F0EA", textColor: "#6E5E57",classNames: ['custom-event'] },
+    //   { id: "5", title: "14:30~18:30", date:"2025-02-20", backgroundColor: "#F7F0EA", textColor: "#6E5E57",classNames: ['custom-event'] },
+    //   { id: "6", title: "18:30~22:30", date:"2025-02-20", backgroundColor: "#F7F0EA", textColor: "#6E5E57",classNames: ['custom-event'] },
     ]);
-   
-  
+    //在API中抓日期
+    useEffect(()=>{
+        (async()=>{
+            try {
+                const res = await axios.get(`${API_URL}scheduleConfig`)
+                const reservedTimeSlots = res.data[0].reservedTimeSlots
+                const newEvents = reservedTimeSlots.map(({ date, timeSlot }) => ({
+                    id: date + timeSlot,
+                    title: timeSlot,
+                    date: date,
+                    backgroundColor: "#F7F0EA",
+                    textColor: "#6E5E57",
+                    classNames: ["custom-event"],
+                  }));
+                setEvents(newEvents)
+                setEventDate(reservedTimeSlots)
+            } catch (error) {
+                console.log(error);
+            }
+        })()
+    },[])
+
     // 將 FullCalendar API 傳遞出去，讓外層能控制
     useImperativeHandle(ref, () => ({
         getApi: () => calendarRef.current?.getApi(),
     }));
     const calendarRef = useRef(null);
+
     const handleDatesSet = (info) => {
         onDateChange(info.view.title);
+        if( eventDate.length !== 0){
+            getCalendarInfo(info,eventDate);
+        }
     };
-    useEffect(() => {
-        if (calendarRef.current) {
-            const calendarApi = calendarRef.current.getApi();
-            onDateChange(calendarApi.view.title);
-        } // 更新當前月份
-    }, [onDateChange]);
 
     const handleEventClick = (info) => {
+        handleCalendar(info)
         // 取消先前選中的事件樣式
         if (selectedEventId) {
             const prevEvent = info.view.calendar.getEventById(selectedEventId);
