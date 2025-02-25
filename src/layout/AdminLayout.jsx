8c8dd15ac19b375d589ffb265b47f48538a64bd2
dieboss <s41115208@gmail.com>
2025-02-26 00:09:48 +0800

    調整後台按鈕

--- src/layout/AdminLayout.jsx
+++ src/layout/AdminLayout.jsx
@@ -4,27 +4,27 @@ export default function AdminLayout() {
   return (
     <div className="bg-neutral-100">
       <div className="container">
-        <nav className="row nav justify-content-center pt-4 mb-4">
-          <div className="nav-item col-sm-6 col-md-4 col-lg-2">
+        <nav className="d-flex nav justify-content-center pt-4 mb-4 gap-md-7 gap-4">
+          <div className="nav-item ">
             <NavLink 
               to='/admin/reservation'
               className={({ isActive }) => 
                 isActive 
-                  ? "nav-link text-center text-dark-grey border-bottom border-2 border-primary fw-bold" 
-                  : "nav-link text-center text-primary-02"
+                  ? "nav-link text-center text-dark-grey border-bottom border-2 border-primary fw-bold fs-3 fs-md-5 px-md-4 py-3 px-8" 
+                  : "nav-link text-center text-primary-02 fw-bold fs-3 fs-md-5 px-md-4 py-3 px-8"
               }
             >
               預約管理
             </NavLink>
           </div>
 
-          <div className="nav-item col-sm-6 col-md-4 col-lg-2">
+          <div className="nav-item ">
             <NavLink 
               to='/admin/orders'
               className={({ isActive }) => 
                 isActive 
-                  ? "nav-link text-center text-dark-grey border-bottom border-2 border-primary fw-bold" 
-                  : "nav-link text-center text-primary-02"
+                  ? "nav-link text-center text-dark-grey border-bottom border-2 border-primary fw-bold fs-3 fs-md-5 px-md-4 py-3 px-8" 
+                  : "nav-link text-center text-primary-02 fw-bold fs-3 fs-md-5 px-md-4 py-3 px-8"
               }
             >
               訂單明細
